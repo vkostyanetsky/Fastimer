9211b5fa0933b36494b0ee56a1511faead78363b
Vlad Kostyanetsky <vlad@kostyanetsky.ru>
2022-07-25 22:52:12 +0700

    Changed fast view

--- fastimer/fast.py
+++ fastimer/fast.py
@@ -12,22 +12,36 @@ def display_fast(fast: dict) -> None:
     time_now = datetime.datetime.now()
 
     if time_now < goal:
-        time = __get_time(time_now, goal)
-        time = f"Remaining:       {time}"
+        remaining_time = __get_time(time_now, goal)
+        excess_time = None
     else:
-        time = __get_time(goal, time_now)
-        time = f"Excess time:     {time}"
+        remaining_time = "00:00"
+        excess_time = __get_time(goal, time_now)
 
     started = fast["started"].strftime("%a, %H:%M")
 
     goal = goal.strftime("%a, %H:%M")
 
-    print(f"Started:  {started}")
-    print(f'Goal:     {goal} ({fast["length"]} hours)')
-    print()
-    print(f"Elapsed time:    {elapsed_time}")
-    print(time)
+    __print_with_alignment("Started", started)
+    __print_with_alignment("Goal", f'{goal} ({fast["length"]} hours)')
 
+    print()
+
+    __print_with_alignment("Elapsed time", elapsed_time)
+    __print_with_alignment("Remaining", remaining_time)
+
+    if excess_time is not None:
+        print()
+        print("You have completed your fast!")
+        print()
+        __print_with_alignment("Excess time", excess_time)
+
+
+def __print_with_alignment(title: str, value: str) -> None:
+    title = f"{title}:".ljust(15)
+    string = "{} {}".format(title, value)
+
+    print(string)
 
 def __get_time(start_date: datetime, end_date: datetime) -> str:
 
